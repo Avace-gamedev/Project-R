bc262dcae0f3dd40c4624f2024224610e5b3713d
Avace <77496973+Avace-gamedev@users.noreply.github.com>
2022-02-27 18:59:16 +0100

    add character config (#6)
    for some reason it was missing from last PR

---
+++ Client.Unity/Assets/Scripts/Configuration/Character/CharacterConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Configuration.Character.Look;
+using Configuration.Character.Movement;
+using UnityEngine;
+
+namespace Configuration.Character
+{
+    [CreateAssetMenu(fileName = "CharacterConfiguration", menuName = "Configuration/Character")]
+    public class CharacterConfiguration : ScriptableObject
+    {
+        public CharacterLookConfiguration look;
+        public CharacterMovementConfiguration movement;
+    }
+}
