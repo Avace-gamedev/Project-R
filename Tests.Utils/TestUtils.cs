9e4ddfc69221ca996056bbb595c63b3e8fe805d7
Avace <77496973+Avace-gamedev@users.noreply.github.com>
2022-02-28 23:47:58 +0100

    add tests

---
+++ Tests.Utils/TestUtils.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System.Reflection;
+
+namespace Tests.Utils;
+
+public static class TestUtils
+{
+    public static string GetTestFile(string testFilePath)
+    {
+        Assembly executingAssembly = Assembly.GetCallingAssembly();
+        string location = Path.GetDirectoryName(executingAssembly.Location)!;
+        string assemblyName = Path.GetFileNameWithoutExtension(executingAssembly.Location);
+        return Path.Combine(location, $"../../../../{assemblyName}/TestFiles", testFilePath);
+    }
+}
